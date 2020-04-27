6eb6a49b222ebc459b6d826fa55a8638dd03c7de
Sergey Gladkovskiy <smgladkovskiy@gmail.com>
2020-04-27 10:13:23 +0300

    Linter image build script fix

--- linter/build.sh
+++ linter/build.sh
@@ -15,7 +15,7 @@ for golang_version in ${go_versions[@]}; do
   echo "Make dockerfile for ${golang_version} and linter version ${GOLANGCI_VERSION}"
   render Dockerfile.tmpl > ${golang_version}/Dockerfile
   cp ./.golangci.yml ${golang_version}
-  docker build -f ${golang_version}/Dockerfile --build-arg -t ${IMAGE}:${golang_version}-${VERSION} ${golang_version}
+  docker build -f ${golang_version}/Dockerfile -t ${IMAGE}:${golang_version}-${VERSION} ${golang_version}
   rm -rf ${golang_version}
 done
 
