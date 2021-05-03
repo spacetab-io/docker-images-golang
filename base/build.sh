b6a2a0dcfc9e04d69dd92ea124672a0c07693bf8
Sergey Gladkovskiy <smgladkovskiy@gmail.com>
2021-05-03 12:13:28 +0300

    expand golang versions to 1.15 and 1.16. Refactoring for linter image building. Versions bump.

--- base/build.sh
+++ base/build.sh
@@ -11,7 +11,7 @@ sed -r "$sedStr" $1
 IFS=' ' read -r -a go_versions <<< ${GOLANG_VERSIONS}
 for golang_version in ${go_versions[@]}; do
   mkdir ${golang_version}
-  echo "Make dockerfile for ${golang_version}"
+  echo "Make base dockerfile for ${golang_version}"
   render Dockerfile.tmpl > ${golang_version}/Dockerfile
   docker build -f ${golang_version}/Dockerfile -t ${IMAGE}:${golang_version}-${VERSION} ${golang_version}
   rm -rf ${golang_version}
--- linter/build.sh
+++ linter/build.sh
@@ -4,6 +4,7 @@ render() {
 sedStr="
   s!%%GOLANG_VERSION%%!${golang_version}!g;
   s!%%GOLANGCI_VERSION%%!$GOLANGCI_VERSION!g;
+  s!%%GOLANGCI_CONFIG_VERSION%%!$GOLANGCI_CONFIG_VERSION!g;
 "
 
 sed -r "$sedStr" $1
@@ -12,9 +13,8 @@ sed -r "$sedStr" $1
 IFS=' ' read -r -a go_versions <<< ${GOLANG_VERSIONS}
 for golang_version in ${go_versions[@]}; do
   mkdir ${golang_version}
-  echo "Make dockerfile for ${golang_version} and linter version ${GOLANGCI_VERSION}"
+  echo "Make linter dockerfile for ${golang_version}, linter version ${GOLANGCI_VERSION} and config for it with version ${GOLANGCI_CONFIG_VERSION}"
   render Dockerfile.tmpl > ${golang_version}/Dockerfile
-  cp ./.golangci.yml ${golang_version}
   docker build -f ${golang_version}/Dockerfile -t ${IMAGE}:${golang_version}-${VERSION} ${golang_version}
   rm -rf ${golang_version}
 done
